51e162f5d63267ee45cdf2e1d5f47948484185d8
tobvc <121837980+tobvc@users.noreply.github.com>
2023-01-07 13:55:31 +0800

    更新 1.js

--- 1.js
+++ 1.js
@@ -1,28 +1,28 @@
-getNetworkType()
-
-function getNetworkType() {
-    var ua = navigator.userAgent;
-    var networkStr = ua.match(/NetType\/\w+/) ? ua.match(/NetType\/\w+/)[0] : 'NetType/other';
-    networkStr = networkStr.toLowerCase().replace('nettype/', '');
-    var networkType;
-    switch(networkStr) {
-        case 'wifi':
-            networkType = 'wifi';
-            break;
-        case '4g':
-            networkType = '4g';
-            break;
-        case '3g':
-            networkType = '3g';
-            break;
-        case '3gnet':
-            networkType = '3g';
-            break;
-        case '2g':
-            networkType = '2g';
-            break;
-        default:
-            networkType = 'other';
-    }
-    alert(networkStr)
-}
+const http = require("http"),
+      fs   = require("fs"),
+      url  = require("url"),
+      path = require("path");
+let server = http.createServer(function(request, response) {
+    let pathObj = url.parse(request.url, true);
+    if (pathObj.pathname === "/" || pathObj === "/index")
+        pathObj.pathname = "/index.html";
+    let filePath = path.join(path.resolve(), pathObj.pathname);
+    let mime = ((ext = path.extname(filePath)) => {
+        let t = {".png": "image/png", ".js": "application/javascript", ".css": "text/css"};
+        return t[ext] || "text/html";
+    })();
+    fs.readFile(filePath, "binary", function(err, fileContent) {
+        if (err) {
+            console.log("404 " + filePath);
+            response.writeHead(404, "not found");
+            response.end("<h1>404 Not Found</h1>");
+        } else {
+            console.log("ok " + filePath);
+            response.setHeader("Content-Type", mime);
+            response.write(fileContent, "binary");
+            response.end();
+        }
+    });
+});
+server.listen(4099);
+console.log('visit http://audbshtuc:4099');
